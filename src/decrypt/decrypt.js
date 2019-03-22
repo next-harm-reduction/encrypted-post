ae791041024016c1bf4932e0a8df5b4379763e34
Schuyler Duveen <sky@skyb.us>
2019-03-22 09:12:23 -0400

    work to making a test page for ease of form changes

--- src/decrypt/decrypt.js
+++ src/decrypt/decrypt.js
@@ -8,33 +8,36 @@ require('datatables.net');
 const decrypt = new JSEncrypt();
 decrypt.setPrivateKey(privKey);
 
-function decryptFormResponses(rows) {
-  function decryptRow(row) {
-    const decryptedStringifiedView = JSON.parse(decrypt.decrypt(atob(row[0])));
-    const view = decryptedStringifiedView.map(str => parseInt(str, 10));
-    const keyBuff = new Uint8Array(view);
-    const fetchKey = crypto.subtle.importKey('raw', keyBuff, 'AES-GCM', false, ['decrypt']);
-    const dataString = atob(row[1]);
-    const dataBuffer = new Uint8Array(dataString.length);
-    for (let i = 0; i < dataString.length; i += 1) {
-      dataBuffer[i] = dataString.charCodeAt(i);
-    }
-    const iv = atob(row[2]).split(',').map(str => parseInt(str, 10));
-    const ivBuffer = new Uint32Array(iv);
-    return fetchKey.then(key => crypto.subtle.decrypt({
-      name: 'AES-GCM',
-      iv: ivBuffer,
-    },
-    key,
-    dataBuffer).then((payload) => {
-      const decoder = new TextDecoder();
-      return JSON.parse(decoder.decode(payload));
-    }).catch((err) => {
-      console.log(err);
-    }));
+function decryptRow(row) {
+  console.log('ROW', row)
+  const decryptedStringifiedView = JSON.parse(decrypt.decrypt(atob(row[0])));
+  const view = decryptedStringifiedView.map(str => parseInt(str, 10));
+  const keyBuff = new Uint8Array(view);
+  const fetchKey = crypto.subtle.importKey('raw', keyBuff, 'AES-GCM', false, ['decrypt']);
+  const dataString = atob(row[1]);
+  const dataBuffer = new Uint8Array(dataString.length);
+  for (let i = 0; i < dataString.length; i += 1) {
+    dataBuffer[i] = dataString.charCodeAt(i);
   }
+  const iv = atob(row[2]).split(',').map(str => parseInt(str, 10));
+  const ivBuffer = new Uint32Array(iv);
+  return fetchKey.then(
+    key => crypto.subtle.decrypt(
+      {
+        name: 'AES-GCM',
+        iv: ivBuffer,
+      },
+      key,
+      dataBuffer).then((payload) => {
+        const decoder = new TextDecoder();
+        return JSON.parse(decoder.decode(payload));
+      }).catch((err) => {
+        console.log(err);
+      }));
+}
+window.decryptRow = decryptRow
 
-
+function decryptFormResponses(rows) {
   return Promise.all(rows.map(decryptRow));
 }
 
@@ -59,13 +62,15 @@ function displayResponses(rows) {
 
 $(document).ready(() => {
   const upload = document.getElementById('encrypted');
-  upload.addEventListener('change', (e) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      const rows = reader.result.split('\n').map(row => row.split(','));
-      //rows.shift(); only if header row -- let's fix this in decryption
-      decryptFormResponses(rows).then(displayResponses);
-    };
-    reader.readAsBinaryString(e.target.files[0]);
-  });
+  if (upload) {
+    upload.addEventListener('change', (e) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const rows = reader.result.split('\n').map(row => row.split(','));
+        //rows.shift(); only if header row -- let's fix this in decryption
+        decryptFormResponses(rows).then(displayResponses);
+      };
+      reader.readAsBinaryString(e.target.files[0]);
+    });
+  }
 });
