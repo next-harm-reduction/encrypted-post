b6ca011fdc8463038494fc8bc5d35b3aab1a685c
Schuyler Duveen <sky@skyb.us>
2019-06-06 08:20:52 -0400

    variables for command context

--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 
 cd "$(dirname "$0")"
 
-SPREADSHEET_ID=1VmcE6WHkF_xWkhCiJGIBnwKF021LwnF7rkpfJlvtOOE
-SUBMIT_URL=https://script.google.com/macros/s/AKfycbwe7OLOLtxAN_smlnNFyQWDqbjVVk9Vq76QwA0Cj8yiX4_SDS7Z/exec
-PUBLIC_KEY_FILE=./rsa_2048_pub.pem
-PRIVATE_KEY_FILE=./rsa_2048_priv.pem
+SPREADSHEET_ID=1VmcE6WHkF_xWkhCiJGIBnwKF021LwnF7rkpfJlvtOOE SUBMIT_URL=https://script.google.com/macros/s/AKfycbwe7OLOLtxAN_smlnNFyQWDqbjVVk9Vq76QwA0Cj8yiX4_SDS7Z/exec PUBLIC_KEY_FILE=./rsa_2048_pub.pem PRIVATE_KEY_FILE=./rsa_2048_priv.pem ./node_modules/.bin/webpack
 
-npm install
 
-./node_modules/.bin/webpack
+
 
