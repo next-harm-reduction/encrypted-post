b29289c9d4bc59bd9db55347507e5c7a56cf0c8a
Schuyler Duveen <sky@skyb.us>
2019-08-20 19:45:07 -0400

    add current public key to project for easy building

---
+++ build_web.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+SPREADSHEET_ID=1VmcE6WHkF_xWkhCiJGIBnwKF021LwnF7rkpfJlvtOOE SUBMIT_URL=https://script.google.com/macros/s/AKfycbwe7OLOLtxAN_smlnNFyQWDqbjVVk9Vq76QwA0Cj8yiX4_SDS7Z/exec PUBLIC_KEY_FILE=./CURRENT_PUBLIC_KEY.pem ./node_modules/.bin/webpack
+
+
+
+
