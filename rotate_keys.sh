f03f99631c19b7a7dfc8db1f93221aaf63903124
Schuyler Duveen <sky@skyb.us>
2019-08-20 20:03:43 -0400

    escape the plus when submitting new public key

--- rotate_keys.sh
+++ rotate_keys.sh
@@ -11,4 +11,4 @@ mv rsa_2048_priv.pem old_keys/rsa_2048_${DATE}_priv.pem
 openssl genrsa -out rsa_2048_priv.pem 2048
 openssl rsa -pubout -in rsa_2048_priv.pem -out rsa_2048_pub.pem
 
-curl -v -X POST -d"date=$DATE"'&'"publicKey=$(cat ./rsa_2048_pub.pem||tr -d '\n')" $SUBMIT_URL
+curl -v -X POST -d"date=$DATE"'&'"publicKey=$(cat ./rsa_2048_pub.pem|sed -e 's/\+/%2B/g'||tr -d '\n')" $SUBMIT_URL
