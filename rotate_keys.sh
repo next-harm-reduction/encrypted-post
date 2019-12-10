94da0824566a7672c82aa9eeb03918f655b7e8e4
Schuyler Duveen <sky@skyb.us>
2019-12-09 22:40:02 -0500

    rotate keys to allow more keys to be local

--- rotate_keys.sh
+++ rotate_keys.sh
@@ -5,10 +5,15 @@ DATE=$(date +'%Y-%m-%d-%H:%M:%S')
 SUBMIT_URL=https://script.google.com/macros/s/AKfycbwe7OLOLtxAN_smlnNFyQWDqbjVVk9Vq76QwA0Cj8yiX4_SDS7Z/exec
 
 mkdir -p old_keys
-mv rsa_2048_pub.pem old_keys/rsa_2048_${DATE}_pub.pem
-mv rsa_2048_priv.pem old_keys/rsa_2048_${DATE}_priv.pem
 
-openssl genrsa -out rsa_2048_priv.pem 2048
-openssl rsa -pubout -in rsa_2048_priv.pem -out rsa_2048_pub.pem
+openssl genrsa -out old_keys/rsa_2048_${DATE}_priv.pem 2048
+openssl rsa -pubout -in old_keys/rsa_2048_${DATE}_priv.pem -out old_keys/rsa_2048_${DATE}_pub.pem
 
+# copy the new public file locally to make it clear which one is current
+cp old_keys/rsa_2048_${DATE}_pub.pem rsa_2048_pub.pem
+
+# aggregate all the private keys into one file so it can be built to decrypt both with old and new keys
+cat old_keys/*_priv.pem > rsa_2048_priv.pem
+
+# upload the public key replacing the '+' which urlencodes as a space, but needs to stay a '+'
 curl -v -X POST -d"date=$DATE"'&'"publicKey=$(cat ./rsa_2048_pub.pem|sed -e 's/\+/%2B/g'||tr -d '\n')" $SUBMIT_URL
